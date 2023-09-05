38cde63b1940e3ab65e567f1ad75e8deda6421cd
DDI112 DDI112 <dorlasry71@gmail.com>
2023-09-05 11:32:57 +0300

    controller handlers create

---
+++ controllers/loginController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    const foundUser = await User.findOne({ username: user }).exec();
+    if (!foundUser) return res.sendStatus(401); //Unauthorized 
+    // evaluate password 
+    // the user was found and we check the password
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+        // need to see if we want to use the roles
+        const roles = Object.values(foundUser.roles).filter(Boolean);
+       
+        // create JWTs
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '40s' } //see if we want more or less
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' } //see if we want more or less
+        );
+        // Saving refreshToken with current user
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+        console.log(roles);
+
+        // Creates Secure Cookie with refresh token
+        res.cookie('jwt', refreshToken, { httpOnly: true,  sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); //secure: true, in prodeuction
+
+        // Send authorization roles and access token to user
+        res.json({ roles, accessToken });
+
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = { handleLogin };
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsConf = require('./config/corsConf');
+const mongoose = require('mongoose');
+const DBconnect = require('./config/DBConnect');
+const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 3500;
+
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const credentials = require('./middleware/credentials');
+
+
+
+
+
+// Connect to MongoDB
+DBconnect();
+
 
 // Cross Origin Resource Sharing
 app.use(cors());
 
 
-
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
